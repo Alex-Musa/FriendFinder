a83c9a3118a9314c64e7c040bff1c1387de5ccb4
alex musa <alex.musa1988@gmail.com>
2019-09-07 11:46:58 -0500

    update new app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,11 @@
 var friends = [
   {
-    name: "Tom Hardy",
+    name: "Ahmed",
     photo:
-      "https://m.media-amazon.com/images/M/MV5BMTQ3ODEyNjA4Nl5BMl5BanBnXkFtZTgwMTE4ODMyMjE@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
-    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
-  {
-    name: "Jamie Dornan",
-    photo:
-      "https://www.usmagazine.com/wp-content/uploads/2018/02/jamie-dornan-nude.jpg",
-    scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
-  },
-
+  
 ];
 module.exports = friends;
+
